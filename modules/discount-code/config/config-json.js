16bdc9df2fcbc438bd4188b303ea675817b78a4b
Suraj8007 <surajughade5@gmail.com>
2024-12-09 17:41:35 +0530

    fixed: update

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -21,7 +21,7 @@ module.exports = function configJSON(req) {
         inArguments: [
           {
             discount: 10,
-            emailAddress: "{{InteractionDefaults.Email}}",
+            emailAddress: "{{contact.Email}}",
           },
         ],
         outArguments: [],
