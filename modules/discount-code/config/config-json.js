7b856d6e2714f7c52651482bef74a6b270995ae1
Suraj8007 <surajughade5@gmail.com>
2024-12-09 15:13:28 +0530

    updated config

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -1,26 +1,28 @@
 module.exports = function configJSON(req) {
   return {
-    workflowApiVersion: '1.1',
+    workflowApiVersion: "1.1",
     metaData: {
       // the location of our icon file
       icon: `images/icon.svg`,
-      category: 'customer'
+      category: "customer",
     },
     // For Custom Activity this must say, "REST"
-    type: 'REST',
+    type: "REST",
     lang: {
-      'en-US': {
-        name: 'Discount Code',
-        description: 'Issue a discount code to contact them to entice them to buy something.'
-      }
+      "en-US": {
+        name: "Discount Code",
+        description:
+          "Issue a discount code to contact them to entice them to buy something.",
+      },
     },
     arguments: {
       execute: {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
           {
-            discount: 10
-          }
+            discount: 10,
+            emailAddress: "{{InteractionDefaults.Email}}",
+          },
         ],
         outArguments: [],
         // Fill in the host with the host that this is running on.
@@ -33,45 +35,52 @@ module.exports = function configJSON(req) {
         // wait in ms between retry.
         retryDelay: 1000,
         // The number of concurrent requests Journey Builder will send all together
-        concurrentRequests: 5
-      }
+        concurrentRequests: 5,
+      },
     },
     configurationArguments: {
       publish: {
-        url: `https://${req.headers.host}/modules/discount-code/publish`
+        url: `https://${req.headers.host}/modules/discount-code/publish`,
       },
       validate: {
-        url: `https://${req.headers.host}/modules/discount-code/validate`
+        url: `https://${req.headers.host}/modules/discount-code/validate`,
       },
       stop: {
-        url: `https://${req.headers.host}/modules/discount-code/stop`
-      }
+        url: `https://${req.headers.host}/modules/discount-code/stop`,
+      },
     },
     userInterfaces: {
-      configurationSupportsReadOnlyMode : true,
+      configurationSupportsReadOnlyMode: true,
       configInspector: {
-        size: 'scm-lg',
-        emptyIframe: true
-      }
+        size: "scm-lg",
+        emptyIframe: true,
+      },
     },
     schema: {
       arguments: {
         execute: {
           inArguments: [],
-          outArguments: [{
-            discountCode: {
-              dataType: 'Text',
-              direction: 'out',
-              access: 'visible'
+          outArguments: [
+            {
+              discountCode: {
+                dataType: "Text",
+                direction: "out",
+                access: "visible",
+              },
+              discount: {
+                dataType: "Number",
+                direction: "out",
+                access: "visible",
+              },
+              emailAddress: {
+                dataType: "Email",
+                isNullable: false,
+                direction: "in",
+              },
             },
-            discount: {
-              dataType: 'Number',
-              direction: 'out',
-              access: 'visible'
-            }
-          }]
-        }
-      }
-    }
+          ],
+        },
+      },
+    },
   };
 };
