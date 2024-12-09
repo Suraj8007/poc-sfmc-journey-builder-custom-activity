165cb8d80d37d16f82d35f51ecc47718f00b1ad1
Suraj8007 <surajughade5@gmail.com>
2024-12-09 17:47:08 +0530

    fixed: update

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -21,7 +21,7 @@ module.exports = function configJSON(req) {
         inArguments: [
           {
             discount: 10,
-            emailAddress: "{{JourneyBuilderCustomActivity_POC_BLR.EmailAddress}}",
+            emailAddress: "{{Contact.EmailAddress}}",
           },
         ],
         outArguments: [],
