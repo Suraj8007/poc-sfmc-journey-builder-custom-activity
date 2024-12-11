2548502c85a3f6fa4ba3f724a61bb70a9d23470b
Suraj8007 <surajughade5@gmail.com>
2024-12-11 14:45:05 +0530

    fixed: update

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -21,9 +21,9 @@ module.exports = function configJSON(req) {
         inArguments: [
           {
             discount: 10,
-            emailAddress: "{{Contact.Attribute.EmailAddress}}",
-            firstName: "{{Contact.Attribute.FirstName}}",
-            
+            emailAddress: "{{Event.DEAudience-8050b33c-84f6-0a49-b6b7-f6744f1565d1.EmailAddress}}",
+            firstName: "'{{Event.DEAudience-8050b33c-84f6-0a49-b6b7-f6744f1565d1.EmailAddress}}",
+
           },
         ],
         outArguments: [],
